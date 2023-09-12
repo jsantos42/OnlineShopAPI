5218c8c18d161c68620358851c4518d5ef3e2c51
João Almeida Santos <79864209+jsantos42@users.noreply.github.com>
2023-09-12 20:11:01 +0200

    feat: add models

---
+++ src/app/Models/Order.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    public function products() {
+        return $this->hasMany(Product::class);
+    }
+
+    public function addProduct(Product $product) {
+        $this->products()->attach($product->id);
+    }
+
+    public function removeProduct(Product $product) {
+        $this->products()->detach($product->id);
+    }
+}
