518acbcc101becb19adf94e8abf1190426430368
João Almeida Santos <79864209+jsantos42@users.noreply.github.com>
2023-09-14 01:42:41 +0200

    feat: add routes (missing auth)

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductCategoryController;
+use App\Http\Controllers\OrderController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/products', [ProductController::class, 'index']);
+
+Route::get('/categories/{id}', [ProductCategoryController::class, 'index']);
+
+Route::get('/customers/{id}/order', [OrderController::class, 'index']);
