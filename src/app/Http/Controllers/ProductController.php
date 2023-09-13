f95ffd4783d8f1c2f6b83687a840819a74ce7533
João Almeida Santos <79864209+jsantos42@users.noreply.github.com>
2023-09-14 01:26:13 +0200

    feat: add products controller

---
+++ src/app/Http/Controllers/ProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class ProductController extends BaseController
+{
+    use AuthorizesRequests, ValidatesRequests;
+
+    public function index()
+    {
+        return response()->json(Product::all());
+    }
+}
