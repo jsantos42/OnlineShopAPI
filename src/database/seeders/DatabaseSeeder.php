aaf9ea91bf58ee90513c18b2e2291b065228c3a5
João Almeida Santos <79864209+jsantos42@users.noreply.github.com>
2023-09-14 00:09:44 +0200

    feat: add remaining models to the seeder

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Database\Factories\OrderFactory;
+use Database\Factories\ProductCategoryFactory;
 use Database\Factories\ProductFactory;
 use Illuminate\Database\Seeder;
 
@@ -18,8 +19,23 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
 
-        OrderFactory::new()->count(3)->create();
-        ProductFactory::new()->count(3)->create();
+        // Creating categories
+        $productCategories = ProductCategoryFactory::new()->count(3)->create();
+
+        // Creating customers and orders
+        $orders = OrderFactory::new()->count(3)->create();
+
+        $orders->each(function ($order) use ($productCategories) {
+            $products = ProductFactory::new()->count(3)->create();
+
+            // Attaching categories to each product
+            $products->each(function ($product) use ($productCategories) {
+                $product->categories()->attach($productCategories->random(rand(1, 3)));
+            });
+
+            // Attaching products to each order
+            $order->products()->attach($products);
+        });
     }
 
 }
