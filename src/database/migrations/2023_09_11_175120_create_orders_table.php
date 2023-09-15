ab2fd6a83eb9703c839999a25f2270c309e3dbfe
João Almeida Santos <79864209+jsantos42@users.noreply.github.com>
2023-09-15 19:44:12 +0200

    feat: add 'placed' field in Orders table

--- src/database/migrations/2023_09_11_175120_create_orders_table.php
+++ src/database/migrations/2023_09_11_175120_create_orders_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->foreignId('customer_id')->constrained();
+            $table->boolean('placed')->default(false);
             $table->timestamps();
         });
     }
